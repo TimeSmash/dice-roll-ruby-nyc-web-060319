8e1190c305924df1e4c6e17d391e9d1ba3d75ae0
mcumm64 <mcumm64@gmail.com>
2019-05-03 23:16:43 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -2,7 +2,12 @@
 
 # Feel free to google "how to generate a random number in ruby"
 
+# def roll
+#   roll_range = (1..6)
+#   rand(roll_range)
+# end
+
 def roll
-  roll_range = (1..6)
-  rand(roll_range)
+  die_sides = [1,2,3,4,5,6]
+  array[rand(0..die_sides.size-1)]
 end
